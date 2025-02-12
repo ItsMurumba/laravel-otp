51db842146d43f30e7ec25f008078ed5c169ad0e
ItsMurumba <kevmurumba@gmail.com>
2025-02-11 21:08:56 -1100

    feat: Adds OtpServiceProvider implementation

--- src/OtpServiceProvider.php
+++ src/OtpServiceProvider.php
@@ -4,6 +4,45 @@ namespace Itsmurumba\Otp;
 
 use Illuminate\Support\ServiceProvider;
 
-class OtpServiceProvider extends ServiceProvider {
-    
+class OtpServiceProvider extends ServiceProvider
+{
+    /**
+     * Publishes all the config file this package needs to function
+     */
+    public function boot()
+    {
+        $config = realpath(__DIR__.'/../resources/config/otp.php');
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                $config => config_path('otp.php'),
+            ], 'otp-config');
+
+            $this->commands([
+                // InstallOtpPackage::class,
+            ]);
+        }
+    }
+
+    /**
+     * Register the application services
+     */
+    public function register()
+    {
+        $this->app->bind('laravel-otp', function () {
+
+            return new Otp();
+        });
+    }
+
+    /**
+     * Get the services provided by the provider
+     *
+     * @return array
+     */
+    public function provides()
+    {
+
+        return ['laravel-otp'];
+    }
 }
